f0cafc9c4f5594c6a9b3c7ca4f5911a7dd2be926
letscodewithrajat <rajat786kanpur@gmail.com>
2025-04-03 16:16:50 +0100

    Initial commit

---
+++ src/main/java/com/rajat/cicd/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.rajat.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rajatsrivastava
+ **/
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, CI/CD with Kubernetes!";
+    }
+}
